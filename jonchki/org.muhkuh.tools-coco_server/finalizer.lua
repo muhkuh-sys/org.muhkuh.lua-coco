6b60a6b70976e6a33746d2eff9e3954c31b90468
Yule1988 <simontru1988@gmail.com>
2021-07-22 16:04:51 +0200

    Add seperate server application

---
+++ jonchki/org.muhkuh.tools-coco_server/finalizer.lua
@@ -0,0 +1,18 @@
+local t = ...
+local pl = t.pl
+
+-- Copy all additional files.
+t:install{
+  -- Copy the server script to the installation base.
+  ['${depack_path_org.muhkuh.lua.coco.lua5.4-coco}/CoCo_server.lua'] = '${install_base}/',
+  ['${depack_path_org.muhkuh.lua.coco.lua5.4-coco}/lua/color_control.lua'] = '${install_base}/lua/',
+  ['${depack_path_org.muhkuh.lua.coco.lua5.4-coco}/lua/color_conversions.lua'] = '${install_base}/lua/',
+  ['${depack_path_org.muhkuh.lua.coco.lua5.4-coco}/lua/tcs_chromaTable.lua'] = '${install_base}/lua/',
+  ['${report_path}']                                       = '${install_base}/.jonchki/'
+}
+
+t:createPackageFile()
+t:createHashFile()
+t:createArchive('${install_base}/../../../${default_archive_name}', 'native')
+
+return true
