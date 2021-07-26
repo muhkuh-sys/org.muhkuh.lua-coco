6a4ec02eee3edd4e9b6722afdfac91b330d4de64
Yule1988 <simontru1988@gmail.com>
2021-07-26 15:45:17 +0200

    Add led_analyzer.so to the finalizer of CoCo server

--- jonchki/org.muhkuh.tools-coco_server/finalizer.lua
+++ jonchki/org.muhkuh.tools-coco_server/finalizer.lua
@@ -4,6 +4,7 @@ local pl = t.pl
 -- Copy all additional files.
 t:install{
   -- Copy the server script to the installation base.
+  ['${depack_path_org.muhkuh.lua.coco.lua5.4-coco}/lua_plugins/led_analyzer.so'] = '${install_base}/lua_plugins',
   ['${depack_path_org.muhkuh.lua.coco.lua5.4-coco}/CoCo_server.lua'] = '${install_base}/',
   ['${depack_path_org.muhkuh.lua.coco.lua5.4-coco}/lua/color_control.lua'] = '${install_base}/lua/',
   ['${depack_path_org.muhkuh.lua.coco.lua5.4-coco}/lua/color_conversions.lua'] = '${install_base}/lua/',
