3094ef43b3acd8ca464a66ef637ac8be684d79b9
Yule1988 <simontru1988@gmail.com>
2021-07-26 14:11:27 +0200

    Adapt installer for CoCo client

--- coco/installer/lua5.4/install_linux.lua
+++ coco/installer/lua5.4/install_linux.lua
@@ -3,7 +3,8 @@ local strDistId, strDistVersion, strCpuArch = t:get_platform()
 local tResult
 
 if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strDistVersion=='@JONCHKI_PLATFORM_DIST_VERSION@' and strCpuArch=='@JONCHKI_PLATFORM_CPU_ARCH@' then
-  t:install('lua_plugins/led_analyzer.so', '${install_lua_cpath}/')
+	t:install('CoCo_client.lua', '${install_lua_path}/')
+	t:install('lua/color_validation.lua', '${install_lua_path}/')
   tResult = true
 end
 
